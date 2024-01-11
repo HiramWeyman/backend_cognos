bf5b6fa1772da4ca622c5c5225461a7c9adba377
Hiram weyman <hiram.wv@gmail.com>
2024-01-10 19:37:44 -0600

    Querys perfiles
    Querys perfiles

--- ApiCognosV1/Repositorio/PacientesRepositorio.cs
+++ ApiCognosV1/Repositorio/PacientesRepositorio.cs
@@ -39,12 +39,14 @@ namespace ApiCognosV1.Repositorio
         public Pacientes GetPacientes(int id)
         { return _bd.Pacientes.FirstOrDefault(p => p.pac_id == id);
         }
-
+         
         public ICollection<Pacientes> GetPacientesList(int id)
         {
             //return _bd.Pacientes.OrderBy(p => p.pac_paterno).ToList();
+           
+            //return _bd.Pacientes.Where(p => p.pac_tutor == id ).OrderBy(p => p.pac_paterno).ToList();
+           return _bd.Pacientes.Where(l => l.pac_tutor == id || l.pac_usr_id == id).OrderBy(p => p.pac_paterno).ToList();
 
-            return _bd.Pacientes.Where(p => p.pac_tutor == id).OrderBy(p => p.pac_paterno).ToList();
         }
 
         public bool Guardar()
